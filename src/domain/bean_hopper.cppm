d2b009a036fabb8f287054c564086d38d53f4ca8
Nathaniel Nicholas <git@nathanielnicholas>
2026-02-26 05:52:36 +0100

    RED. add doesn't throw when coarse beans are used.

--- src/domain/bean_hopper.cppm
+++ src/domain/bean_hopper.cppm
@@ -1,5 +1,6 @@
 module; /* Global Module Fragment. */
 
+#include <optional>
 #include <stdexcept>
 
 export module BeanHopper;  /* Interface Unit. */
@@ -14,12 +15,25 @@ public:
     this->capacity_ = capacity;
   }
 
+  std::optional<CoffeeBeans> add(const CoffeeBeans &beans);
+
+  double grams() const;
+
   double capacity() const;
 
 private:
   double capacity_;
+  std::optional<CoffeeBeans> beans_;
 };
 
 module :private; /* Implementation Unit. */
+
+std::optional<CoffeeBeans> BeanHopper::add(const CoffeeBeans &beans) {
+  return std::nullopt;
+}
+
+double BeanHopper::grams() const {
+  return this->beans_ ? this->beans_->grams() : 0.0;
+}
 
 double BeanHopper::capacity() const { return this->capacity_; }
--- tests/domain/bean_hopper_tests.cpp
+++ tests/domain/bean_hopper_tests.cpp
@@ -1,7 +1,28 @@
 #include <gtest/gtest.h>
+#include <print>
 import BeanHopper;
+
+namespace {
+
+CoffeeBeans light_roast() {
+  return CoffeeBeans{15.0, CoffeeRoast::light, CoffeeGrind::whole};
+}
+
+CoffeeBeans dark_roast() {
+  return CoffeeBeans{15.0, CoffeeRoast::dark, CoffeeGrind::whole};
+}
+
+} // namespace
 
 TEST(BeanHopper, BeanHopper_Should_Throw_If_Capacity_Negative) {
   double illegal_capacity = -200.0;
   ASSERT_THROW((BeanHopper{illegal_capacity}), std::runtime_error);
+}
+
+TEST(BeanHopper, add_Should_Throw_If_CoffeeGrind_Is_Not_Whole) {
+  double capacity = 200.0;
+  BeanHopper bh{capacity};
+  auto beans = light_roast();
+  beans.set_grind(CoffeeGrind::coarse);
+  ASSERT_THROW(bh.add(beans), std::runtime_error);
 }
