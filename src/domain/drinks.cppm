649ad25f08b58dee00db6b6f9da496d1a5704114
Nathaniel Nicholas <git@nathanielnicholas>
2026-02-26 09:57:52 +0100

    RED. volume() doesn't return total volume

--- src/domain/drinks.cppm
+++ src/domain/drinks.cppm
@@ -11,9 +11,19 @@ export enum class Coffee : std::uint8_t { americano, espresso, macchiato };
 export class Drink {
 public:
   void add(Liquid liquid);
+
+  std::vector<Liquid> liquids() const;
+
+  double volume() const;
+
+private:
   std::vector<Liquid> liquids_;
 };
 
 module :private; /* Implementation Unit. */
 
 void Drink::add(Liquid liquid) { this->liquids_.push_back(liquid); }
+
+std::vector<Liquid> Drink::liquids() const { return this->liquids_; }
+
+double Drink::volume() const { return -1; }
--- tests/domain/drinks_tests.cpp
+++ tests/domain/drinks_tests.cpp
@@ -9,5 +9,22 @@ TEST(Drinks, add_Should_Append_Liquid_To_Vector) {
   Liquid milk{volume, temperature};
   drink.add(std::move(milk));
 
-  ASSERT_EQ(drink.liquids_.size(), 1);
+  ASSERT_EQ(drink.liquids().size(), 1);
+}
+
+TEST(Drinks, volume_Should_Return_Total_Volume) {
+  Drink drink;
+
+  double milk_volume = 120.0;
+  double milk_temperature = 4.0;
+  Liquid milk{milk_volume, milk_temperature};
+  drink.add(std::move(milk));
+
+  double espresso_volume = 30.0;
+  double espresso_temperature = 90.0;
+  Liquid espresso{espresso_volume, espresso_temperature};
+  drink.add(std::move(espresso));
+
+  double total_volume = milk_volume + espresso_volume;
+  ASSERT_EQ(drink.volume(), total_volume);
 }
